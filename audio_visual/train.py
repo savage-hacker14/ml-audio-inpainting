f75d6ce009ac960aa9bb4fc5a00c51f74fe3c792
Jacob Krucinski <jacob1576@gmail.com>
2025-03-24 20:16:28 -0400

    Added test/train split

--- audio_visual/train.py
+++ audio_visual/train.py
@@ -2,7 +2,7 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, random_split
 
 import tqdm
 import configparser
@@ -27,11 +27,12 @@ model.to(device)
 
 # Create data loader
 dataset = LibriSpeechDataset(LIBRISPEECH_ROOT_PROCESSED)
-data_loader = DataLoader(dataset, batch_size=8, shuffle=True)
+train_dataset, test_dataset = random_split(dataset, [config['p_train'], config['p_test']])
+train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True)
 
 # Define loss function and optimizer
 criterion = nn.L1Loss()
-optimizer = optim.Adam(model.parameters(), lr=0.0005)
+optimizer = optim.Adam(model.parameters(), lr=0.001)
 
 # Define number of train epochs
 num_epochs = 5
@@ -39,9 +40,9 @@ for epoch in range(num_epochs):
     model.train()
     running_loss = 0.0
 
-    pbar = tqdm.trange(len(data_loader), desc=f"Epoch {epoch+1}/{num_epochs}")
-    for batch_idx in pbar:
-        inputs, targets = next(iter(data_loader))
+    pbar = tqdm.tqdm(train_loader, desc=f"Epoch {epoch+1}/{num_epochs}")
+    for batch_idx, (inputs, targets) in enumerate(pbar):
+        inputs, targets = next(iter(train_loader))
 
         inputs = inputs.to(device)
         targets = targets.to(device)
@@ -64,7 +65,8 @@ for epoch in range(num_epochs):
         #     print(f"Epoch [{epoch+1}/{num_epochs}], Step [{batch_idx+1}], Loss: {loss.item():.4f}")
         pbar.set_description(f"Epoch [{epoch+1}/{num_epochs}], Step [{batch_idx+1}], Loss: {loss.item():.4f}")
 
-    avg_loss = running_loss / len(data_loader)
+    avg_loss = running_loss / len(train_loader)
     print(f"Epoch [{epoch+1}/{num_epochs}], Average Loss: {avg_loss:.4f}")
+    torch.save(model.state_dict(), f"checkpoints/blstm_epoch_{epoch+1}_2025_03_24.pt")
 
 print("Training Complete!")
